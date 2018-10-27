f9b9863a2d47a53e791845b1e2d58aa14fe58b82
Vitor Castro <v.j.r.c1997@hotmail.com>
2018-10-27 20:09:06 +0100

    Builded jar

--- src/script/Code.java
+++ src/script/Code.java
@@ -29,10 +29,10 @@ public class Code {
             return true;
 
         } catch (MalformedURLException e) {
-            System.err.println("Exceção...");
+            //System.err.println("Exceção...");
             throw new RuntimeException(e);
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            //System.err.println(e.getMessage());
             return false;
         }
     }
@@ -48,69 +48,69 @@ public class Code {
             // entering random website will lead us to the login page of nosfon
             Process p = Runtime.getRuntime().exec(new String[]{"explorer", "http://www.sapo.pt"});
             // wait 30 seconds and close the browser after that
-            System.out.println("Sleeping 30 sec");
+            //System.out.println("Sleeping 30 sec");
             Thread.sleep(30000);
             Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
 
         } catch (InterruptedException e) {
-            System.err.println("Exceção...");
+            //System.err.println("Exceção...");
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            //System.err.println(e.getMessage());
         }
     }
 
     public void testing() {
 
-        System.out.println("$ LAUNCHING TESTING.");
+        //System.out.println("$ LAUNCHING TESTING.");
         int nrAttempts = 0;
 
         while (true) {
 
             try {
-                System.out.println("A dormir 20 segundos.");
+                //System.out.println("A dormir 20 segundos.");
                 Thread.sleep(20000);
                 if (this.netIsAvailable() == true) {
                     nrAttempts = 0;
-                    System.out.println("Há net... Tudo bem.");
+                    //System.out.println("Há net... Tudo bem.");
                 } else {
                     nrAttempts++;
                     // try for 10 minutes to connect to NOS FON
                     if (nrAttempts < 30) {
-                        System.out.println("Ligar à net FON");
+                        //System.out.println("Ligar à net FON");
                         Runtime.getRuntime().exec("netsh wlan connect ssid=NOS_WIFI_Fon name=NOS_WIFI_Fon interface=\"Wi-Fi 3\"");
                         execBrowser();
                     }
                     // if passes more than that connect to serjo
                     else {
-                        System.out.println("Ligar à net serjo");
+                        //System.out.println("Ligar à net serjo");
                         Runtime.getRuntime().exec("netsh wlan connect ssid=Queres net? Paga! name=Queres net? Paga! interface=\"Wi-Fi 3\"");
                         // execBrowser(); -> no need to open browser since we dont need to login in a webpage
                     }
                 }
 
             } catch (InterruptedException e) {
-                System.err.println("Exceção...");
+                //System.err.println("Exceção...");
             } catch (IOException e) {
-                System.err.println(e.getMessage());
+                //System.err.println(e.getMessage());
             }
         }
     }
 
     public void startup() {
 
-        System.out.println("$ LAUNCHING STARTUP.");
+        //System.out.println("$ LAUNCHING STARTUP.");
 
         try {
             if (this.netIsAvailable() == true) {
-                System.out.println("Há net... Tudo bem.");
+                //System.out.println("Há net... Tudo bem.");
             } else {
-                System.out.println("Ligar à net FON");
+                //System.out.println("Ligar à net FON");
                 Runtime.getRuntime().exec("netsh wlan connect ssid=NOS_WIFI_Fon name=NOS_WIFI_Fon interface=\"Wi-Fi 3\"");
                 execBrowser();
             }
 
         } catch (IOException e) {
-            System.err.println(e.getMessage());
+            //System.err.println(e.getMessage());
         }
     }
 }
